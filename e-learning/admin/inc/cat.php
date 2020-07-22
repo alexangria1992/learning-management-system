f6d091765e27d2e33753d9cd1ed26081aac78321
alexangria1992 <55860445+alexangria1992@users.noreply.github.com>
2020-07-22 12:27:30 +1200

    Created and Designed Category form

--- e-learning/admin/inc/cat.php
+++ e-learning/admin/inc/cat.php
@@ -1,3 +1,13 @@
 <div id="bodyright">
     <h3>View All Categories</h3>
+    <div id="add">
+        <details>
+            <summary>Add Category</summary>
+        <form method="post" enctype="multipart/form-data">
+             <input type="text" name="cat_name" placeholder="Enter Category Name Here">
+             <center><button name="add_cat">Add Category</button></center>
+       </form>
+       </details>
+
+    </div>
 </div>
